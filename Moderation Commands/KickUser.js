5c5084db0f13d664965ade17f73cfec8cd101698
AtomixDem <74184968+AtomixDem@users.noreply.github.com>
2021-04-29 15:19:35 +0200

    Create KickUser.js

---
+++ Moderation Commands/KickUser.js
@@ -0,0 +1,42 @@
+/*
+Discord bot script for send a auto message
+Script by AtomixDem -> https://github.com/AtomixDem
+*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("PASTE YOUR TOKEN HERE"); // Paste your Token Bot here
+
+// Commands
+client.on("message", (message) => {
+    if(message.content.startsWith("!kick")) { // Your command
+
+        var userKick = message.mentions.members.first();
+
+        if(!message.member.hasPermission("KICK_MEMBERS")) { // Permission required
+            message.channel.send("You don't have the permission!");
+            return;
+        }
+        
+        if(!userKick) {
+            message.channel.send("You haven't mentioned any users!");
+            return;
+        }
+
+        if(!userKick.kickable) {
+            message.channel.send("I can not do that!");
+            return;
+        }
+
+        var embed = new Discord.MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Kicked user!")
+            .addField("User:", "<@" + userKick + ">", false)
+
+
+        userKick.kick()
+            .then(() => message.channel.send(embed)) // Bot message
+
+    }
+})
