e6c0360a8cef7a36b95ff69eaa8f4e6bb0c36723
AtomixDem <74184968+AtomixDem@users.noreply.github.com>
2021-04-29 10:10:29 +0200

    Create SendTime.js

---
+++ Basic Commands/SendTime.js
@@ -0,0 +1,20 @@
+/*
+Discord bot script for basic commands
+Script by AtomixDem -> https://github.com/AtomixDem
+*/
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.login("PASTE YOUR TOKEN HERE"); // Paste your Token Bot here
+
+
+bot.on("message", (message) =>{ // Send time
+    if(message.content == '!time'){
+        var date = new Date();
+        var hour = data.getHours();
+        var minute = data.getMinutes();
+
+        message.channel.send(":alarm_clock: Ora attuale: " + hour + ":" + minute); // Message with the time (:alarm_clock: is the emoji clock)
+    }
+})
