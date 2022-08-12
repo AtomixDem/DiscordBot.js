7d3c77eba56a523d741c9bc8fda7972e3ec7dcc0
AtomixDem <74184968+AtomixDem@users.noreply.github.com>
2022-08-12 16:58:25 +0200

    Create UpdateOne.js

---
+++ MongoDB/UpdateOne.js
@@ -0,0 +1,19 @@
+/*
+Discord bot script for update an object in the Collection
+Script by AtomixDem -> https://github.com/AtomixDem
+*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const MongoClient = require("mongodb").MongoClient; // Install MongoDB library => npm install mongodb
+
+client.login("PASTE YOUR TOKEN HERE"); // Paste your Token Bot here
+
+// Connection
+var urldb = "mongodb+srv://YOUR_USERNAME:YOUR_PASSWORD@*********.*********.mongodb.net/test" // Paste your MongoDB link here
+    MongoClient.connect(urldb, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+        var database = db.db("COLLECTION_NAME");
+
+        database.collection("COLLECTION_NAME").updateOne({id:"1234"}, {$set:{username:"Test2"}}) // Update an object
+
+    })
